6d976bd2abb2d14dfb44b91105a1dc6fc8a836c5
Jarek9 <jbabecki@wp.pl>
2017-12-09 13:19:44 +0100

    Tasks

---
+++ src/main/java/com/crud/tasks/com/crud/tasks/domain/TaskDto.java
@@ -0,0 +1,18 @@
+package com.crud.tasks.com.crud.tasks.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TaskDto {
+    private Long id;
+    private String title;
+    private String content;
+
+    @Override
+    public String toString() {
+        return  id + " " + title + " " + content;
+    }
+}
