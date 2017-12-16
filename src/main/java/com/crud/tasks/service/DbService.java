eccb4e333e7920336df34adf5d9474fb6bf2f61c
Jarek9 <jbabecki@wp.pl>
2017-12-16 16:02:26 +0100

    Task_15.2.1

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class DbService {
@@ -17,6 +16,6 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Optional<Task> getTask(final Long id) {
-        return repository.findById(id);}
+    public Task getTaskById(final Long id) {
+        return repository.findById(id).orElse(null);}
 }
